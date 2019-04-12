ae4d61cb076aa1c06b8b384dfc512cc2f7b226b8
Wojciech S. Gac <wojciech.s.gac@gmail.com>
2019-04-12 15:49:41 +0200

    Approach the problem of drawing lines

--- src/machine-independent-graphics.lisp
+++ src/machine-independent-graphics.lisp
@@ -10,27 +10,42 @@
 ;;;; Theory".
 
 (defparameter *plot-window* nil)
+(defparameter *graphics-context* nil)
 
 ;; Some CLX examples: http://www.cawtech.demon.co.uk/clx/simple/examples.html
 
 (defun init-plot (&key (title "Plot Window") (x-size 500) (y-size 500))
+  "Initialize an emtpy Xorg window. Return the window object."
   (let* ((display (open-default-display))
 	 (screen (car (display-roots display)))
-	 (root-window (screen-root screen)))
+	 (root-window (screen-root screen))
+         (white (screen-white-pixel screen)))
     (setf *plot-window*
 	  (create-window :parent root-window
 			 :x 0
 			 :y 0
 			 :width x-size
 			 :height y-size
-			 :background :parent-relative
+			 :background white
 			 ))
     (change-property *plot-window*
 		     :wm_name title
 		     :string 8)
     (map-window *plot-window*)
     (display-finish-output display)
+    (init-graphics-context)
     *plot-window*))
+
+(defun init-graphics-context ()
+  (let* ((display (open-default-display))
+         (screen (car (display-roots display)))
+         (black (screen-black-pixel screen))
+         (white (screen-white-pixel screen)))
+   (setf *graphics-context*
+         (create-gcontext
+          :drawable *plot-window*
+          :foreground white
+          :background black))))
 
 (defun plot-fill-rect (x y xsize ysize pattern)
   (declare (ignorable x y xsize ysize pattern)))
@@ -38,7 +53,10 @@
 (defun plot-size-rect (x y xsize ysize val)
   (declare (ignorable x y xsize ysize val)))
 
-(defun clear-plot ())
+(defun clear-plot (&key (window *plot-window*))
+  (clear-area window
+              :width (drawable-width window)
+              :height (drawable-height window)))
 
 (defun pen-width (nibs)
   (declare (ignorable nibs)))
@@ -47,7 +65,26 @@
   (declare (ignorable x y xsize ysize)))
 
 (defun plot-line (x1 y1 x2 y2)
-  (declare (ignorable x1 y1 x2 y2)))
+  ""
+  (let* ((display (open-default-display))
+         (screen (car (display-roots display)))
+         ;; (white (screen-white-pixel screen))
+         (black (screen-black-pixel screen)))
+    (map-window *plot-window*)
+    (setf (gcontext-foreground *graphics-context*) black)
+    (event-case (display :force-output-p t
+                         :discard-p t)
+      (:exposure ()
+                 (break)
+                 (draw-line *plot-window*
+                            *graphics-context*
+                            x1 y1 x2 y2)
+                 nil)
+      (:button-press () t)
+      (:key-press (code state) (char= #\Space (xlib:keycode->character display code state))))
+
+    (display-finish-output display)
+    (close-display display)))
 
 (defun show-plot ())
 
